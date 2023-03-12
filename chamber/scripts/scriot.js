b35a07471ececc8790954a4230f87cd38fbfb818
paulagyare.github.io-WDD230- <biggstagga@gmail.com>
2023-03-12 01:35:23 +0000

    correction for respnosive pagr

--- chamber/scripts/scriot.js
+++ chamber/scripts/scriot.js
@@ -12,6 +12,33 @@ dateBar.textContent = `Welcome, Today's Date is ${formattedDate}.`;
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navigationMenu = document.querySelector('.navigation-menu');
 
+// Add event listener for hamburger menu click
 hamburgerMenu.addEventListener('click', () => {
   navigationMenu.classList.toggle('show');
 });
+
+// Add event listener for window resizing
+window.addEventListener('resize', () => {
+  // Get viewport width
+  const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+  // Check if viewport is mobile or tablet size
+  if (viewportWidth < 768) {
+    // Adjust CSS styles for mobile and tablet viewports
+    navigationMenu.style.display = 'none';
+    hamburgerMenu.style.display = 'block';
+  } else {
+    // Reset CSS styles for larger viewports
+    navigationMenu.style.display = 'flex';
+    hamburgerMenu.style.display = 'none';
+  }
+});
+
+// Set initial CSS styles based on viewport size
+const initialViewportWidth = window.innerWidth || document.documentElement.clientWidth;
+if (initialViewportWidth < 768) {
+  navigationMenu.style.display = 'none';
+  hamburgerMenu.style.display = 'block';
+} else {
+  navigationMenu.style.display = 'flex';
+  hamburgerMenu.style.display = 'none';
+}
